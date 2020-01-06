e9c7781a61a55017ed605188ad69c4d98f5f43c5
BrunoG.B <brunocavalcantedsilva@gmail.com>
2020-01-06 14:07:58 -0300

    Primeiro commit

---
+++ src/App.Business/Models/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Business.Models
+{
+    class Entity
+    {
+        public Guid Id { get; set; }
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
