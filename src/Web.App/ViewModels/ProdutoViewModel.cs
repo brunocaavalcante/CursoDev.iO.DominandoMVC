432a62c605ac82fdc0620e53b8b0c8e9b9e8a2b1
bruno <brunocavalcantedsilva@gmail.com>
2020-01-12 14:02:52 -0300

    Mapeando classes com automapping

---
+++ src/Web.App/ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.App.ViewModels
+{
+    public class ProdutoViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage ="O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage ="O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength =2)]
+        public string Nome { get; set; }
+
+        [DisplayName("Descrição")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public IFormFile ImagemUpload { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public decimal Valor { get; set; }
+
+        [ScaffoldColumn(false)] //Para não ser criado como um campo pois ele vai desconsiderar pois a data será ensirida automaticamente
+        public DateTime DataCadastro { get; set; }
+
+        [DisplayName("Ativo?")]
+        public bool Ativo { get; set; }
+
+        /* EF relations*/
+        public FornecedorViewModel Fornecedor { get; set; }
+    }
+}
