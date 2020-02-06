f62907e9202bc57cbb717f24a9c73e6324338e76
BrunoGW <brunocavalcantedsilva@gmail.com>
2020-02-06 12:12:51 -0300

    Globalização da cultura da aplicação para pt-BR

---
+++ src/Presentation/Extensions/MoedaAttribute.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Presentation.Extensions
+{
+    public class MoedaAttribute : ValidationAttribute
+    {
+        //Validação lado Service
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            try
+            {
+                var moeda = Convert.ToDecimal(value, new CultureInfo("pt-BR"));
+            }
+            catch (Exception)
+            {
+                return new ValidationResult("Moeda no formato inválido.");
+            }
+            return ValidationResult.Success;
+        }
+    }
+
+    public class MoedaAttributeAdapter : AttributeAdapterBase<MoedaAttribute>
+    {
+        public MoedaAttributeAdapter(MoedaAttribute attribute, IStringLocalizer stringLocalizer) : base(attribute, stringLocalizer)
+        {
+
+        }
+        public override void AddValidation(ClientModelValidationContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            MergeAttribute(context.Attributes, "data-val", "true"); //Atribuindo valor ao atributos do contexto
+            MergeAttribute(context.Attributes, "data-val-moeda", GetErrorMessage(context));
+            MergeAttribute(context.Attributes, "data-val-number", GetErrorMessage(context));
+        }
+
+        public override string GetErrorMessage(ModelValidationContextBase validationContext)
+        {
+            return "Moeda em formato inválido.";
+        }
+    }
+}
